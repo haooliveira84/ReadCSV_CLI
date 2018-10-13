408ee8aef46a3ec2af062277bb1aea05cc89747e
Henrique Oliveira <haooliveira@gmail.com>
2018-10-13 20:11:35 -0300

    Add function for result

--- app.py
+++ app.py
@@ -13,3 +13,14 @@ from collections import Counter
 
 cont = Counter()
 
+def main_process(json_data):
+    values = []
+    data = sorted(json_data, key=lambda k: k['estado'])
+    for value in data:
+        values.append((value['estado'], value['nome']))
+    for estado, nome in values:
+        cont[estado] += 1
+        customers = sorted(cont.items())
+    return jsonify(customers)
+
+if __name__ == "__main__":
