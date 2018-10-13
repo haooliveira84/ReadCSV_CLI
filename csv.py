02ac63f276ac036998d832331972a025784319bc
Henrique Oliveira <haooliveira@gmail.com>
2018-10-13 20:25:05 -0300

    Add csv read funcition

--- csv.py
+++ csv.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
 import csv
+import sys
+import requests
+import csv
+
+def get_csvfile(url):
+    r = requests.get(url)
+    text = r.iter_lines()
+    reader = csv.reader(text, delimiter=',')
+
+def main():
+    url = sys.argv[1]
+
+if __name__ == "__main__":
+    main()
