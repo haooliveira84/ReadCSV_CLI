0e834ac271d233d0caadab0e4795e08d9c5a2bd8
Henrique Oliveira <haooliveira@gmail.com>
2018-10-13 22:57:01 -0300

    Add functions for remote and local files

---
+++ csv_reader.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import requests
+import csv
+
+def get_csvfile_remote(file):
+    r = requests.get(file)
+    text = r.iter_lines()
+    reader = csv.reader(text, delimiter=',')
+    for row in reader:
+        print row
+
+def get_csvfile_local(file):
+    with open(file) as text:
+        reader = csv.reader(text, delimiter=',')
+        for row in reader:
+            print row
+
+def main():
+    file = sys.argv[1]
+    if file.startswith('htt'):
+        get_csvfile_remote(file)
+    else:
+        get_csvfile_local(file)
+
+if __name__ == "__main__":
+    main()
