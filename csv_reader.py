ccdce8b653d5a313a69a917576af4ec553587911
Henrique Oliveira <haooliveira@gmail.com>
2018-10-16 01:15:23 -0300

    Fiz skip first line on reader csv

--- csv_reader.py
+++ csv_reader.py
@@ -7,14 +7,15 @@ import json
 
 def get_csvfile_remote(file):
     r = requests.get(file)
-    text = r.iter_lines()
-    reader = csv.DictReader(text, delimiter=',',fieldnames = ("nome","cidade","estado"))
+    data = r.iter_lines()
+    reader = csv.DictReader(data, delimiter=',',fieldnames = ("nome","cidade","estado"))
     out = json.dumps( [ row for row in reader ] )
     return out
 
 def get_csvfile_local(file):
-    with open(file) as text:
-        reader = csv.DictReader(text, delimiter=',',fieldnames = ("nome","cidade","estado"))
+    with open(file) as data:
+        reader = csv.DictReader(data, delimiter=',',fieldnames = ("nome","cidade","estado"))
+        next(reader)
         out = json.dumps( [ row for row in reader ] )
     return out
 
